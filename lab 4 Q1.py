8f51f188493239ef71fababf4339ef642ea54b18
ElicitWeb <lalitmrao@gmail.com>
2023-09-14 14:36:48 +0530

    lab questions

--- lab 4 Q1.py
+++ lab 4 Q1.py
@@ -1,10 +1,27 @@
+# def series1(n):
+#     Sum = 0
+#     fact = 1
+#
+#     for i in range(1, n + 1):
+#         fact *= i
+#         Sum += i / fact
+#
+#     return Sum
+#
+#
+# n = int(input("Enter the number of terms: "))
+# answer = series1(n)
+# print(f"The sum of the series for {n} terms is: {answer}")
+
 def series1(n):
     Sum = 0
     fact = 1
+    i = 1
 
-    for i in range(1, n + 1):
+    while i <= n:
         fact *= i
         Sum += i / fact
+        i += 1
 
     return Sum
 
--- lab 4 Q4.py
+++ lab 4 Q4.py
@@ -1,11 +1,31 @@
+# def series4(x, n):
+#     sum = 0
+#     fact = 1
+#
+#     for i in range(1, n + 1):
+#         fact *= i
+#         if fact % 2 == 0:
+#             sum += x / fact
+#
+#     return sum
+#
+#
+# x = float(input("Enter the value of x: "))
+# n = int(input("Enter the number of terms: "))
+# answer = series4(x, n)
+# finalAnswer = x + answer
+# print(f"The sum of the series is: {finalAnswer}")
+
 def series4(x, n):
     sum = 0
-    factorial = 1
+    fact = 1
+    i = 1
 
-    for i in range(1, n + 1):
-        factorial *= i
-        if factorial % 2 == 0:
-            sum += x / factorial
+    while i <= n:
+        fact *= i
+        if fact % 2 == 0:
+            sum += x / fact
+            i += 1
 
     return sum
 
